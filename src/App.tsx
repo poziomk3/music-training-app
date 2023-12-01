be8319195227e0cf99eb61b669ed12dfd4357e84
Karol Zając <karolzajac.0407@gmail.com>
2023-12-01 20:51:40 +0100

    styled circle of fifths

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import Triangle from "./components/Triangle";
+import CircleOfFifths from "./components/CircleOfFifths";
 
 function App() {
-  return (
-    <div className="bg-red-200 w-full h-screen flex justify-center items-center overflow-hidden">
-      
-      <div className="relative ">
-      
-        {Array(12)
-          .fill(0)
-          .map(
-            (item, index) => (
-             
-              <div key={index} 
+  // const obj1= useFrequency({
+  //   hz: 400,
+  //   type:"center",
+  //   gain: 10/ 100,
+  //   oscillator:"sine"
+  // })
+  // const obj2 = useFrequency({
+  //   hz: 1950,
+  //   type:"center",
+  //   gain: 10/ 100,
+  //   oscillator:"sine"
+  // })
 
-                style={{ transformOrigin:"bottom", top:0, left:0, transform:`translate(-50%,-100%) rotate(${5+index *30}deg)  `, transitionDuration:'300ms'  }}
-              className='absolute   w-[100px] z-[1]   ' 
-              >
-                <div className="hover:scale-125 duration-300  w-[20px]  scale-[.80]">
-                  <div className="w-[100px] "><Triangle /></div>
-                  
-                </div>
-              </div>
-            )
-           
-          )}
-      </div>
-      <div className="absolute w-[100px] aspect-square bg-blue-700 rounded-full z-20  "/>
-     
+  return (
+    <div className=" w-full min-h-screen h-[100vh]  ">
+      <CircleOfFifths />
+      {/* <CircleOfFifths/> */}
     </div>
   );
 }
---
+++ src/components/CircleOfFifths.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react";
+import Triangle from "./Triangle";
+
+interface CircleOfFifthsProps {}
+interface Note {
+  name: string;
+  color: string;
+}
+const cirlceOfFifths: Array<Note> = [
+  { name: "C", color: "#6A0DAD" },
+  { name: "G", color: "#5D0C9E" },
+  { name: "D", color: "#500B8F" },
+  { name: "A", color: "#430A80" },
+  { name: "E", color: "#360970" },
+  { name: "B", color: "#290861" },
+  { name: "F#", color: "#1C0752" },
+  { name: "C#", color: "#0F0643" },
+  { name: "G#", color: "#020534" },
+  { name: "D#", color: "#000425" },
+  { name: "A#", color: "#000316" },
+  { name: "F", color: "#000207" },
+];
+
+const CircleOfFifths: FC<CircleOfFifthsProps> = () => {
+  return (
+    <div className="relative h-full w-full   bg-red-200 flex justify-center items-center overflow-hidden">
+      <div className="absolute ">
+          {cirlceOfFifths.map((item, index) => (
+            <div
+              key={index}
+              style={{
+                transformOrigin: "bottom",
+                top: 0,
+                left: 0,
+                transform: `translate(-50%,-100%) rotate(${5 + index * 30}deg)  `,
+                transitionDuration: "300ms",
+              }}
+              className="absolute   w-[100px] z-[1]   "
+            >
+              <div className="hover:scale-95  duration-300  w-[100px] aspect-square z-[20] relative  ">
+                <div
+                  style={{
+                    transformOrigin: "center",
+                    transform: `translate(50%,50%) rotate(-${5 + index * 30}deg) `,
+                  }}
+                  className="cursor-pointer absolute z-20 text-white right-1/2 text-[2rem] font-bold"
+                >
+                  {item.name}
+                </div>
+                <Triangle fillColor={item.color} />
+              </div>
+            </div>
+          ))}
+          <div className="absolute w-[220px] -translate-x-1/2 -translate-y-1/2 aspect-square opacity-80 bg-[#1C0752] rounded-full z-20  " />
+      </div>
+    </div>
+  );
+};
+
+export default CircleOfFifths;
